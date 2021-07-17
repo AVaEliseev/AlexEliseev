4fc6a63c5a06410b4ddb9f9545b935572259a2f0
eliseew.av@gmail.com <eliseew.av@gmail.com>
2021-07-17 13:46:37 +0300

    Update project Lesson7

--- Lesson7_maven/src/main/java/additionValue.java
+++ Lesson7_maven/src/main/java/additionValue.java
@@ -1,6 +1,6 @@
 public class additionValue {
     @org.testng.annotations.DataProvider(name = "additionValue")
     public static Object [][] additionValueMethod(){
-        return new Object[][]{{2, 3, 5}, {4, 5, 9}, {5, 5, 10}, {25, 15, 40}};
+        return new Object[][]{{5, 3, 8}, {4, 5, 9}, {5, 5, 10}, {25, 15, 40}};
     }
 }
